0474a76ba378f6b6c74b6e2cedd331a3c1090133
Marcus Guidoti <marcus.guidoti@gmail.com>
2019-07-06 19:44:47 -0300

    feat: Add InfoWindow feature

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -103,7 +103,7 @@ function initMap() {
 
     // Create the infowindow object, that will show information from
     // Google Maps and Foursquare APIs.
-    let infowindow = new google.maps.InfoWindow();
+    let infoWindow = new google.maps.InfoWindow();
 
     // Set the color of the default markers.
     const defaultIcon = makeMarkerIcon('00ff00');
@@ -128,7 +128,8 @@ function initMap() {
           title: myPlaces[i].title,
           icon: defaultIcon,
           animation: google.maps.Animation.DROP,
-          id: myPlaces[i].googleMaps_ID
+          id: myPlaces[i].googleMaps_ID,
+          foursquare_id: myPlaces[i].foursquare_ID
         });
 
         // Push the marker to the array of markers.
@@ -138,7 +139,8 @@ function initMap() {
         // clicked (that's what the 'this' part is doing here).
         marker.addListener('click', function() {
             this.setIcon(clickedIcon);
-            populateInfoWindow(this, infowindow);
+            myInfoWindow(this, infoWindow);
+            map.setCenter(marker.position);
         });
 
         // Adds two event listeners, one for mouseover and one for mouseout
@@ -174,3 +176,115 @@ function initMap() {
         return markerImage;
     }
 }
+
+function myInfoWindow(marker, infowindow) {
+    
+    let service = new google.maps.places.PlacesService(map);
+    
+    let infoWindowContent = '<div id="infowindow">';
+
+    // Set the marker property on this infowindow so it isn't created again
+    infowindow.marker = marker;
+
+    // Make data request to Google Maps API
+    service.getDetails({
+        placeId: marker.id
+    }, function(place, status) {
+    
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+        
+            // Add the information from Google Maps API
+            if (place.name) {
+                infoWindowContent += '<h4>' + place.name + '</h4>';
+            }
+            if (place.formatted_address) {
+                infoWindowContent += '<p><span class="infowindow-type">' +
+                    'Address:</span> ' + 
+                    '<span class="infowindow-data">' + 
+                    place.formatted_address + 
+                    '</span></p>';
+            }
+            if (place.international_phone_number) {
+                infoWindowContent += '<p><span class="infowindow-type">' +
+                    'International Phone Number:</span> ' + 
+                    '<span class="infowindow-data">' + 
+                    place.international_phone_number +
+                    '</span></p>';
+            }
+            if (place.opening_hours) {
+                infoWindowContent += '<p><span class="infowindow-type">' +
+                    'Open now?</span> ' +
+                    '<span class="infowindow-data">' + 
+                    place.opening_hours.open_now +
+                    '</span></p>';
+            }
+            if (place.url) {
+                infoWindowContent += '<p><span class="infowindow-type">' +
+                    'Google Maps URL:</span> ' +
+                    '<span class="infowindow-data">' +
+                    '<a href=' + 
+                    place.url +
+                    '>' + 
+                    place.url +
+                    '</a></span></p>';
+            }
+        
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function() {
+                infowindow.marker = null;
+            });
+        } else {
+            infoWindowContent += '<p><span class="infowindow-type">' +
+            'Google Maps API information could not be loaded</span>';
+        }
+
+        // AJAX Request for venue tips on Foursquare
+        const foursquareClientID = 'VK31XCCX1Q4K2BDABYZNB3WEQLRCG2V0VGKJFDZ20GBJCEYS';
+        const foursquareClientSecret = 'EDA0P4IS2O1HTJBZN0S31XQCCDULNDEUNABBOLSKXWD3PHHB';
+
+        // Building Foursquare API URL
+        let foursquareUrlAPI = 'https://api.foursquare.com/v2/venues/' +
+            marker.foursquare_id + 
+            '/tips?client_id=' +
+            foursquareClientID + '&client_secret=' + 
+            foursquareClientSecret + 
+            '&v=20160118';
+        
+        // Actual AJAX request
+        $.getJSON(foursquareUrlAPI, function(data) {
+
+            // Gets data if the API call response is successfull
+            if (data["meta"]["code"] == 200) {
+                
+                // Check if there is any tip on this response
+                if (data["response"]["tips"]["items"].length > 0) {
+
+                    infoWindowContent += "<br><h4>Foursquare #1 Tip</h4>";
+                    
+                    infoWindowContent += '<p><span class="infowindow-data">"' +
+                        data["response"]["tips"]["items"][0]["text"] +
+                        '"</span></p>';
+                    
+                    infoWindowContent += '<p id="infowindow-4sq-author"><span class="infowindow-type">' +
+                        data["response"]["tips"]["items"][0]["user"]["firstName"] +
+                        ' </span>';
+                    
+                    infoWindowContent += '<span class="infowindow-data">(Likes: ' +
+                        data["response"]["tips"]["count"] +
+                        ')</span></p>';  
+                } else {
+                    infoWindowContent += "<br><h4>Foursquare has no Tips for this location</h4>";
+                }
+            } 
+        }).fail(function() {
+            infoWindowContent += '<p><span class="infowindow-type">' +
+            'Foursquare API data could not be loaded</span>';
+        }).always(function() {
+            infoWindowContent += '</div>';
+            infowindow.setContent(infoWindowContent);
+            infowindow.open(map, marker);
+        });
+    });
+}
+
+
